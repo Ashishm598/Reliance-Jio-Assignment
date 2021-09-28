d4c4092a7c49f6c4be61a0118483d6bc3c3f2111
Ashish Mishra <72121532+ashishmishraaig@users.noreply.github.com>
2021-09-29 03:16:39 +0530

    [added] Response Models

---
+++ app/src/main/java/com/android/jio/model/Data.kt
@@ -0,0 +1,9 @@
+package com.android.jio.model
+
+data class Data(
+    val avatar: String,
+    val email: String,
+    val first_name: String,
+    val id: Int = 0,
+    val last_name: String
+)
---
+++ app/src/main/java/com/android/jio/model/Support.kt
@@ -0,0 +1,6 @@
+package com.android.jio.model
+
+data class Support(
+    val text: String,
+    val url: String
+)
